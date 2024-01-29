517340f0b91d8dc575ebcc57044dbc742b103806
Rasikakale16 <66539908+Rasikakale16@users.noreply.github.com>
2024-01-29 12:02:41 +0530

    added code

--- cart.js
+++ cart.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://addcart-5f551-default-rtdb.firebaseio.com/"
@@ -20,15 +20,96 @@ addButtonEl.addEventListener("click", function() {
 
     clearInputFieldEl()
     
-    appendItemToShoppingListEl(inputValue)
+    //appendItemToShoppingListEl(inputValue)
     
     //shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
+
+
+
+
+
+
+onValue(shoppingListInDB,function(snapshot){
+    //console.log(snapshot.val());
+    if(snapshot.exists()){
+        let itemsArray = Object.entries(snapshot.val())
+    //console.log(itemsArray)
+
+    // console.log(snapshot.val());
+
+    clearShoppingListEl()
+    //shoppingListEl.innerHTML= ""
+
+    for(let i = 0; i< itemsArray.length;i++){
+        //itemsArray[i]
+        let currentitem = itemsArray[i]
+
+        let currentitemID = currentitem[0]
+        let currentitemValue = currentitem[1]
+        
+        //appendItemToShoppingListEl(currentitemValue)
+        appendItemToShoppingListEl(currentitem)
+        //appendItemToShoppingListEl(itemsArray[i])
+
+        //console.log(itemsArray[i]);
+
+    }
+    } else{
+        shoppingListEl.innerHTML ="yet, No itemsss!🧐"
+    }
+
+
+    // let itemsArray = Object.entries(snapshot.val())
+    // //console.log(itemsArray)
+
+    // // console.log(snapshot.val());
+
+    // clearShoppingListEl()
+    // //shoppingListEl.innerHTML= ""
+
+    // for(let i = 0; i< itemsArray.length;i++){
+    //     //itemsArray[i]
+    //     let currentitem = itemsArray[i]
+
+    //     let currentitemID = currentitem[0]
+    //     let currentitemValue = currentitem[1]
+        
+    //     //appendItemToShoppingListEl(currentitemValue)
+    //     appendItemToShoppingListEl(currentitem)
+    //     //appendItemToShoppingListEl(itemsArray[i])
+
+    //     //console.log(itemsArray[i]);
+
+    // }
+
+
+})
+
+function clearShoppingListEl(){
+    shoppingListEl.innerHTML= ""
+}
 
 function clearInputFieldEl(){
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(inputValue){
-    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+function appendItemToShoppingListEl(item){   //item replaces itemValue
+    //shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function() { 
+        let exactLocationofItemInDB = ref(database,`shoppingList/${itemID}`)
+        //console.log(itemID);
+
+        remove(exactLocationofItemInDB) 
+    })
+
+    shoppingListEl.append(newEl)
 }
